9be94d91d7d1c2d0d7babc7f9eef68f063e17f58
Nathaly Gomez Lopez <nath.gomez.lopez@gmail.com>
2023-08-24 12:07:53 +0200

    Seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,9 @@ Booking.destroy_all
 Place.destroy_all
 User.destroy_all
 
-puts 'Creating 1 user and user places'
+# Seed pour Nathaly
+
+puts 'Creating 1 user(Nathaly)'
 
 user1 = User.create!(
   first_name: "Nathaly",
@@ -22,28 +24,9 @@ user1 = User.create!(
   password: "123456"
 )
 
-# place1 = user1.places.create!(
-#   name: Faker::Restaurant.name,
-#   address: Faker::Address.full_address,
-#   price: Faker::Number.within(range: 400..1000),
-#   picture_url: "https://source.unsplash.com/random"
-# )
-
-url = "https://api.unsplash.com/photos/random?client_id=#{ENV["ACCESS_KEY"]}&query=bar"
-photo_serialized = URI.open(url).read
-photo_json = JSON.parse(photo_serialized)
-photo_url = photo_json["urls"]["small"]
-file = URI.open(photo_url)
-place1 = user1.places.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, price: rand(400..1000))
-# Attach the photo using your Cloudinary config
-place1.photo.attach(io: file, filename: "bar.png", content_type: "image/png")
-place1.save!
-
-
-puts 'Creating 1 seed via unplash'
-
-# Build the URL for your query to unsplash
-# with your access_key and a query for some keywords:
+puts 'Creating 3 places for Nathaly via unplash'
+  # Build the URL for your query to unsplash
+  # with your access_key and a query for some keywords:
 url = "https://api.unsplash.com/photos/random?client_id=#{ENV["ACCESS_KEY"]}&query=bar"
 
 # Fetch this URL, it will return a json containing infos about 1 random photo
@@ -54,11 +37,25 @@ photo_url = photo_json["urls"]["small"]
 
 # Download this photo and save it into a variable
 file = URI.open(photo_url)
-place2 = user1.places.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, price: rand(400..1000))
+place1 = user1.places.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, price: rand(400..1000))
 # Attach the photo using your Cloudinary config
-place2.photo.attach(io: file, filename: "bar.png", content_type: "image/png")
-place2.save!
+place1.photo.attach(io: file, filename: "bar.png", content_type: "image/png")
+place1.save!
 
+
+puts 'Creating 6 bookings for places Nathaly via unplash'
+6.times do
+  booking = Booking.new(
+    status: ["pending confirmation", "booked", "refused"].sample,
+    begin_date: Faker::Date.between(from: '2022-09-23', to: '2023-01-13'),
+    end_date: Faker::Date.between(from: '2023-01-14', to: Date.today)
+  )
+booking.user = user1
+booking.place = place1
+booking.save!
+end
+
+# Seed pour les fakes users
 puts 'Creating 10 fake users and fake places via unsplash'
 
 10.times do
@@ -77,61 +74,23 @@ puts 'Creating 10 fake users and fake places via unsplash'
   )
   user.save!
 
-  place = user.places.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, price: rand(400..1000))
-  place.photo.attach(io: file, filename: "bar.png", content_type: "image/png")
-  place.save!
-end
+  place2 = user.places.new(name: Faker::Restaurant.name, address: Faker::Address.full_address, price: rand(400..1000))
+  place2.photo.attach(io: file, filename: "bar.png", content_type: "image/png")
+  place2.save!
 
-# 10.times do
-#   user = User.new(
-#     first_name: Faker::Name.first_name,
-#     last_name: Faker::Name.last_name,
-#     birth_date: Faker::Date.birthday,
-#     email: Faker::Internet.email,
-#     password: Faker::Internet.password
-#   )
-#   user.save!
-#   user.places.create!(
-#     name: Faker::Restaurant.name,
-#     address: Faker::Address.full_address,
-#     price: Faker::Number.within(range: 400..1000),
-#     picture_url: "https://source.unsplash.com/random"
-#   )
-# end
+  puts 'Creating 5 bookings '
 
-puts 'Creating 3 bookings '
-
-3.times do
-  booking = Booking.new(
-    status: ["pending confirmation"].sample,
-    begin_date: Faker::Date.between(from: '2022-09-23', to: '2023-01-13'),
-    end_date: Faker::Date.between(from: '2023-01-14', to: Date.today)
-  )
-  booking.user = User.where.not(id: user1).sample
-  booking.place = place1
-  booking.save!
-end
-
-2.times do
-  booking = Booking.new(
-    status: ["booked","pending confirmation"].sample,
-    begin_date: Faker::Date.between(from: '2022-09-23', to: '2023-01-13'),
-    end_date: Faker::Date.between(from: '2023-01-14', to: Date.today)
-  )
+  3.times do
+    booking = Booking.new(
+      status: ["pending confirmation", "booked", "refused"].sample,
+      begin_date: Faker::Date.between(from: '2022-09-23', to: '2023-01-13'),
+      end_date: Faker::Date.between(from: '2023-01-14', to: Date.today)
+      )
   booking.user = User.where.not(id: user1).sample
   booking.place = place2
   booking.save!
+  end
 end
 
-10.times do
-booking = Booking.new(
-  status: ["pending confirmation", "booked", "refused"].sample,
-  begin_date: Faker::Date.between(from: '2022-09-23', to: '2023-01-13'),
-  end_date: Faker::Date.between(from: '2023-01-14', to: Date.today)
-)
-booking.user = user1
-booking.place = place1
-booking.save!
-end
 
 puts 'Seed finished'
