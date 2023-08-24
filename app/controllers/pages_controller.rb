e4ade0051b4d8320803f5afbc06de14aa1c64847
michel tran <tran.michel1@gmail.com>
2023-08-24 12:52:50 +0200

    Search Bar + affichage  User Place et Other User Place

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
   def home
     @places_user = Place.where(user_id: current_user)
+    @places_otheruser = Place.where.not(user_id: current_user)
     @places = Place.all
+    if params[:query].present?
+      # Méthode Search Active Record
+      # @places = @places.where(name: params[:query])
+      @places = Place.search_by_name_and_address(params[:query])
+      @places_otheruser = @places_otheruser.search_by_name_and_address(params[:query])
+    end
   end
 end
